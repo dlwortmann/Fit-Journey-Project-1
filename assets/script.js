4b82e2a479ca6fa246083689282a89425509aef6
Jpeterson <peterson.jacob13@protonmail.com>
2025-01-23 20:36:54 -0600

    completed first function

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+$(document).foundation();
+
 // gets user input elements
 const sleepIntakeEl = document.getElementById('sleep-intake')
 const waterIntakeEl = document.getElementById('water-intake')
@@ -10,16 +12,81 @@ foodSlectorEle = document.getElementById('food-selector')
 cardioSelectorEle = document.getElementById('cardio-selector')
 weightsSelectorEle = document.getElementById('weights-selector')
 
-const getValues = () =>{
-    // get amout of slee/water/food/excercise
-    const hrOfSleep = sleepIntakeEl.value
-    const ozOfWater = waterIntakeEl.value
+// store calories eaten and burned
+let totalCalsEaten = 0
+let totalCalsBurned = 0
+
+const compareValues = () =>{
+    // get amout of /food/excercise
     const gramOfFood = foodIntakeEl.value
-    const minuteOfCardio = cardioInputEl.value
+    const minutesOfCardio = cardioInputEl.value
     const minutesOfWeights = weightsInputEl.value
 
     // get type of food/excercise
     const typeOfFood = foodSlectorEle.value
     const typeOfCardio = cardioSelectorEle.value
     const typeOfWeights = weightsSelectorEle.value
+
+    if(sleepIntakeEl.value === '' || waterIntakeEl.value === '' || foodIntakeEl.value === '' || cardioInputEl.value === '' || weightsInputEl.value === ''){
+        $('#error-1').foundation('reveal', 'open')
+    }
+
+    if(typeOfFood && foodIntakeEl.value === '' || typeOfCardio && cardioInputEl.value === '' || typeOfWeights && weightsInputEl.value === ''){
+        $('#error-2').foundation('reveal', 'open')
+    }
+
+    let calories;
+    let caloriesBurned;
+
+    // calculate calories eaten
+    switch(typeOfFood){
+        case 'Meat':
+            calories = gramOfFood * 6.5
+            break;
+        case 'Vegetables':
+            calories = gramOfFood * 5
+            break;
+        case 'fats':
+            calories = gramOfFood * 9
+            break;
+        case 'sugars':
+            calories = gramOfFood * 4
+        default:
+            break;
+    }
+    totalCalsEaten += calories
+
+    // calculate calories burned
+    switch(typeOfCardio){
+        case 'Walking':
+            caloriesBurned = minutesOfCardio * 6
+            break;
+        case 'Running':
+            caloriesBurned = minutesOfCardio * 14
+            break;
+        case 'Swimming':
+            caloriesBurned = minutesOfCardio * 6.5
+            break;
+        case 'Jumprope':
+            caloriesBurned = minutesOfCardio * 15
+            break;        
+        default:
+            break;
+    }
+    totalCalsBurned += caloriesBurned
+
+    switch(typeOfWeights){
+        case 'Bench Press':
+            caloriesBurned = minutesOfWeights * 5
+            break;
+        case 'Squats':
+            caloriesBurned = minutesOfWeights * 8
+            break;
+        case 'Deadlift':
+            caloriesBurned = minutesOfWeights * 7
+            break;
+        default:
+            break;
+    }
+    totalCalsBurned += caloriesBurned
 }
